bd64b3149ba10d9bf7fe4ebbc1a2fd10f5a2ed2d
Behrad Assa <behradassa@gmail.com>
2025-04-20 23:57:47 -0400

    implemeneted aes-128

--- GCS_MASTER.ino
+++ GCS_MASTER.ino
@@ -2,13 +2,46 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Arduino.h>
+#include <AES.h>
+#include <AESLib.h>
+#include <AES_config.h>
+#include <string.h>
+#include <Base64.h>
 
 //can receive a maximum of 33 byte payload
 //ground on esp32
 RF24 radio(21, 22); // CE, CSN
+AES aes;
 uint8_t txAddress[] = "gndrec";
 uint8_t rxAddress[] = "mslrec";
 char reply[32];
+
+byte cipher[32];
+byte msg_out[32];
+String key="yeahhhhbabyyyyyy";// 16 bytes
+String iv="bigbyomboballs77"; //16 bytes
+
+void do_encrypt(byte msg[32], String key_str, String iv_str) {
+  byte iv[16];
+  memcpy(iv, iv_str.c_str(), 16);
+
+  byte key[16];
+  memcpy(key, key_str.c_str(), 16);
+
+  aes.set_key(key, 16);
+  aes.cbc_encrypt(msg, cipher, 2, iv);  // 32 bytes = 2 AES blocks
+}
+
+void do_decrypt(byte cipher[32], String key_str, String iv_str) {
+  byte iv[16];
+  memcpy(iv, iv_str.c_str(), 16);
+
+  byte key[16];
+  memcpy(key, key_str.c_str(), 16);
+
+  aes.set_key(key, 16);
+  aes.cbc_decrypt(cipher, msg_out, 2, iv);  // 2 blocks = 32 bytes
+}
 
 void setup() {
   Serial.begin(115200);
@@ -17,7 +50,6 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);
   radio.openReadingPipe(1, rxAddress);
   radio.openWritingPipe(txAddress);
-  radio.setAutoAck(1, true);
 }
 
 void loop() {
@@ -30,8 +62,15 @@ void loop() {
     char request[33];
     input.toCharArray(request, sizeof(request));
 
-
-    radio.write(&request, sizeof(request));
+    byte msg[32] = {
+        0xDE, 0xAD, 0xBE, 0xEF, 0x11, 0x22, 0x33, 0x44,
+        0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC,
+        0x01, 0x02, 0x03, 0x04, 0xA0, 0xB0, 0xC0, 0xD0,
+        0xE0, 0xF0, 0x00, 0x10, 0x20, 0x30, 0x40, 0x50
+      };
+    
+    do_encrypt(msg, key, iv);
+    radio.write(&cipher, sizeof(cipher));
     Serial.print("Sent: ");
     Serial.println(input);
     
@@ -45,15 +84,20 @@ void loop() {
     while (millis() - startTime < 350) {
       if (radio.available()) {
         radio.read(&reply, sizeof(reply));
-        Serial.print("Received: ");
-        Serial.println(reply);
+         Serial.print("Received: ");
+          for (int i = 0; i < 32; i++) {  // adjust based on how many bytes you send
+            Serial.print("0x");
+            Serial.print(reply[i], HEX);
+            Serial.print(" ");
+          }
+          Serial.println();
         timeout = false;
         break;
-      } 
+      }
     }
 
     if (timeout) {
-      Serial.println("No reply received------------------Retry-------------------");
+      Serial.println("No reply received-------------------------------------");
     }
 
 }
--- MSL_SLAVE.ino
+++ MSL_SLAVE.ino
@@ -1,40 +1,83 @@
+#define byte uint8_t  // Fix ambiguity
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h> 
+#include <AES.h>
+#include <AESLib.h>
+#include <AES_config.h>
+#include <string.h>
 
-using namespace std;
-char recv_req[32];
-//msl on arduino
-RF24 radio(9, 10); //CE CSN pins to arduino, needs to be esp32 for more current
+RF24 radio(21, 22); // CE, CSN
+
 uint8_t txAddress[] = "gndrec";
 uint8_t rxAddress[] = "mslrec";
 
+AES aes;
+uint8_t recv_req[32];
+uint8_t msg_out[32];
+
+String key_str = "yeahhhhbabyyyyyy";  // 16 bytes
+String iv_str  = "bigbyomboballs77";  // 16 bytes
+
+void do_encrypt(uint8_t msg[32], String key_str, String iv_str) {
+  uint8_t iv[16];
+  memcpy(iv, iv_str.c_str(), 16);
+
+  uint8_t key[16];
+  memcpy(key, key_str.c_str(), 16);
+
+  aes.set_key(key, 16);
+  aes.cbc_encrypt(msg, msg_out, 2, iv);  // output to msg_out
+}
+
+void do_decrypt(uint8_t cipher[32], String key_str, String iv_str) {
+  uint8_t iv[16];
+  memcpy(iv, iv_str.c_str(), 16);
+
+  uint8_t key[16];
+  memcpy(key, key_str.c_str(), 16);
+
+  aes.set_key(key, 16);
+  aes.cbc_decrypt(cipher, msg_out, 2, iv);
+}
+
 void setup() {
   Serial.begin(115200);
-  //init radio settings
   radio.begin();
   radio.setDataRate(RF24_250KBPS);
   radio.setPALevel(RF24_PA_MIN);
   radio.openWritingPipe(rxAddress);
   radio.openReadingPipe(1, txAddress);
   radio.startListening();
-  radio.setAutoAck(1, true);
-
 }
 
 void loop() {
   if (radio.available()) {
     radio.read(&recv_req, sizeof(recv_req));
-    Serial.print("Got: ");
-    Serial.println(recv_req);
+
+    Serial.print("Got encrypted: ");
+    for (int i = 0; i < 32; i++) {
+      Serial.print("0x");
+      Serial.print(recv_req[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+
+    do_decrypt(recv_req, key_str, iv_str);
+
+    Serial.print("Decrypted: ");
+    for (int i = 0; i < 32; i++) {
+      Serial.print("0x");
+      Serial.print(msg_out[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
 
     radio.stopListening();
-    delayMicroseconds(150); 
-    const char response[] = "DATA_REPLY";
+    delay(80); // Simulate processing
+    radio.write(&recv_req, sizeof(recv_req));
     Serial.println("Sent DATA_REPLY");
-    radio.write(&recv_req, sizeof(recv_req)); 
     delayMicroseconds(150);
     radio.startListening();
   }
-} 
-
+}
